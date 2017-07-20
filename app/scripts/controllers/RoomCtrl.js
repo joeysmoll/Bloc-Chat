7840a93e10c9827e0f0b093549518961b5f1a7c7
joeysmoll <jsmoll91@gmail.com>
2017-07-19 17:50:09 -0700

    Finished listing messages

--- app/scripts/controllers/RoomCtrl.js
+++ app/scripts/controllers/RoomCtrl.js
@@ -1,16 +1,28 @@
 (function() {
-    function RoomCtrl(Room, $uibModal) {
+    function RoomCtrl(Room, Message, $uibModal) {
         this.allRooms = Room.all;
 
         this.openModal = function() {
            var instance = $uibModal.open({
                templateUrl:'/templates/modal.html',
                 controller: 'ModalCtrl',
-                controllerAs: 'modal'}) 
+                controllerAs: 'modal'}); 
         };
+
+        this.selectedRoom = null;
+
+        this.selectedMessages = null;
+
+        this.selectRoom = function(room){
+            this.selectedRoom = room;
+            console.log(this.selectedRoom);
+            this.selectedMessages = Message.getByRoomId(room.$id);
+            console.log(this.selectedMessages);
+        };
+
     }
 
     angular
         .module('blocChat')
-        .controller('RoomCtrl', ['Room','$uibModal', RoomCtrl]);
+        .controller('RoomCtrl', ['Room', 'Message', '$uibModal', RoomCtrl]);
 })();
